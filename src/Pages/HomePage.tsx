f3b749cfd1debbec7e00fbdf59f8b845d153f89f
Umar535 <syedumarali535@gmail.com>
2025-07-09 12:19:24 +0500

    footer-secure-spot

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,11 +1,15 @@
+import Footer from '../Components/Footer'
 import Headers from '../Components/Headers'
 import MenuShowcase from '../Components/MenuShowCase'
+import SecureSpot from '../Components/SecureSpot'
 
 function HomePage() {
   return (
     <div>
        <Headers />
        <MenuShowcase />
+       <SecureSpot/>
+       <Footer />
     </div>
   )
 }
