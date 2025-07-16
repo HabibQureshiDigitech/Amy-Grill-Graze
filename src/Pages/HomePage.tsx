3660a25475a93d5dc544c6e83699ff681f5593eb
HabibQureshiDigitech <habib@digitechinfra.com>
2025-07-16 10:10:29 +0500

    habib-commit

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,14 +1,10 @@
-
 import Crafting from '../Components/Crafting'
 import Footer from '../Components/Footer'
-// import Headers from '../Components/Headers'
-// import MenuShowcase from '../Components/MenuShowCase'
 import Savor from '../Components/Savor'
 import SecureSpot from '../Components/SecureSpot'
-
 import ExploreHistory from "../Components/ExploreHistory"
-// import Headers from "../Components/Headers"
-// import MenuShowcase from "../Components/MenuShowCase"
+import Headers from "../Components/Headers"
+import MenuShowcase from "../Components/MenuShowCase"
 import SeasonalSpecials from "../Components/SeasonalSpecials"
 
 
